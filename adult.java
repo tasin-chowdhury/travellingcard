e75d07ddf4878c26a92518a2f33c4f2ddf784fcf
Tasin Tahiat Chowdhury <47826467+tasin-chowdhury@users.noreply.github.com>
2020-04-09 09:55:22 +0600

    Update adult.java

--- adult.java
+++ adult.java
@@ -12,7 +12,7 @@ public class adult extends Person {
     
     @Override
     public void getinformation(){
-        System.out.println("Name is : "+name+"\ngender is "+gender +"\nage is "+age +"\nnationality is "+nationality);
+        System.out.println("Name is : "+name+"\nGender is "+gender +"\nAge is "+age +"\nNationality is "+nationality);
 
         System.out.println(name+ "'s NID is : "+NID);
     }  

a3b44d0be45ceaa3a17c0d1f41b5fe348160ad04
Tasin Tahiat Chowdhury <47826467+tasin-chowdhury@users.noreply.github.com>
2020-04-09 09:49:56 +0600

    Create Infant.java

---
+++ Infant.java
@@ -0,0 +1,22 @@
+package travellingcard;
+
+public class Infant extends Person {
+    private String Birthcertificate,parentNID;
+    public Infant(String name, String gender, String age, String nationality,String Birthcertificate,String parentNID){
+     super(name,gender,age,nationality);
+     this.Birthcertificate=Birthcertificate;
+     this.parentNID=parentNID;
+    }
+    public void setBirthcertificate(String Birthcertificate){
+        this.Birthcertificate=Birthcertificate;
+    }
+    public void setparentNID(String parentNID){
+        this.parentNID=parentNID;
+    }
+    @Override
+    public void getinformation(){
+        System.out.println("Name is : "+name+"\ngender is "+gender +"\nage is "+age +"\nnationality is "+nationality);
+        System.out.println(name+"'s Birth Certificate No : "+Birthcertificate +"\n"+name+ "'s parent NID No :"+parentNID);
+    }
+    
+}
